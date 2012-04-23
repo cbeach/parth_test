3d60b4a1e7cb0baea79db22390f881e66d5e5b52
Casey Beach <beachc@gmail.com>
2012-04-23 02:20:51 -0700

    added instructions to README

--- publisher.py
+++ publisher.py
@@ -1,15 +1,15 @@
 import socket, json, time
 
 class Publisher:
-    def __init__(self):
+    def __init__(self,host,port):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.connect(('localhost',8080))
+        self.sock.connect((host,port))
         self.sock.send( json.dumps({'command':'publisher'}))
 
     def publish(self, data):
         self.sock.send(json.dumps({'body':data}))
 
-p = Publisher() 
+p = Publisher('localhost',8080) 
 
 while True:
     line = raw_input()
--- subscriber.py
+++ subscriber.py
@@ -2,9 +2,9 @@ import socket, json
 
 
 class Subscriber:
-    def __init__(self):
+    def __init__(self,host,port):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.connect(('localhost',8080))
+        self.sock.connect((host,port))
         self.sock.send( json.dumps({'command':'subscriber'}))
 
     def start_listening(self, callback):
@@ -18,7 +18,7 @@ class Subscriber:
 def print_message(data):
     print(data)
 
-s = Subscriber()
+s = Subscriber('localhost',8080)
 s.start_listening(print_message)
             
 
